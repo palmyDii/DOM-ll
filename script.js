73140daaf639788307bcbf5dc1d77d2b1e5ca69c
nicha.satt <nicha.satt@gmail.com>
2022-05-20 11:50:59 +0700

    good array

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-let students = []
 let student = {} 
 student.name = 'คุณลุง'
 student.username = 'a@b.com'
 student.gender = 'ชาย'
-students.push(student)
 
 let student2 = {
     name : 'คุณป้า',
     username : 'b@a.com',
     gender : 'หญิง' 
 }
-students.push(student2)
 
-//document.getElementById('output').innerText = student.name
+let students = [
+    student,
+    student2,
+    {
+        name : 'คุณน้า',
+        username : 'b@c.com',
+        gender : 'หญิง'  
+    }
+    
+]
 
 function addStudentData(students) {
     for (let stu of students) {
@@ -30,6 +36,7 @@ function addDataForm(student, topic, needValue) {
     let columnName = document.createElement('div')
     columnName.classList.add('col-1')
     columnName.classList.add('offset-1')
+    columnName.classList.add('fw-bold')
     columnName.innerHTML = topic
 
     let columnValue = document.createElement('div')
